4272d6c66e04965c3917cf8101becbdd4010d653
Albert Hoe <albertjameshoe@gmail.com>
2026-03-26 11:30:30 -0400

    .

--- hw5/startup.sh
+++ hw5/startup.sh
@@ -8,11 +8,12 @@ gcloud auth application-default login --no-launch-browser
 gcloud auth application-default print-access-token
 
 apt-get update -y
-apt-get install -y python3-pip git
+apt-get install -y python3-pip python3-venv git
 
+python3 -m venv /opt/venv
+source /opt/venv/bin/activate
 
 git clone https://github.com/albhoe/528.git /opt/528
-pip3 install --break-system-packages -r /opt/528/hw5/requirements.txt
-export BUCKET_NAME='alhoe528hw2'
-export PORT=8080
+pip install -r /opt/528/hw5/requirements.txt
+
 nohup python3 /opt/528/hw5/hw5server.py > /root/server.log 2>&1 &
