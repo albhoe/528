a17e5b04c1c6b9a25403d7f4f5f41627f3e14570
Albert Hoe <albertjameshoe@gmail.com>
2026-03-26 15:02:20 -0400

    .

--- hw5/cleanup.sh
+++ hw5/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PROJECT_ID="bucsece528"
-REGION='us-central1'
-ZONE='us-central1-a'
+REGION='us-east5'
+ZONE='us-east5-a'
 
 gcloud compute instances delete hw4-forbidden --zone=$ZONE --quiet
 gcloud compute instances delete hw4-webserver --zone=$ZONE --quiet
