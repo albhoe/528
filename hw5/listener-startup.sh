7c171f6d52dac1be85e9b23f869f44c1f767555b
Albert Hoe <albertjameshoe@gmail.com>
2026-03-26 04:04:47 -0400

    .

--- hw5/listener-startup.sh
+++ hw5/listener-startup.sh
@@ -3,6 +3,7 @@ exec > /var/log/startup-script.log 2>&1
 set -x
 
 apt-get update -y
+sudo apt-get install --reinstall ca-certificates
 apt-get install -y python3-pip git
 git clone https://github.com/albhoe/528.git /opt/528
 pip3 install --break-system-packages -r /opt/528/hw5/requirements.txt
