7943450154348882de23bf0c5db3d4c817509c57
Albert Hoe <albertjameshoe@gmail.com>
2026-04-02 08:41:45 -0400

    .

--- hw6/modelvm.sh
+++ hw6/modelvm.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
 exec > /var/log/startup-script.log 2>&1
-set -x
+set -ex
 
 if [ -f /var/log/startup_already_done ]; then
    exit 0
 fi
 
-echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
+echo 'Acquire::ForceIPv4 "true";' | tee /etc/apt/apt.conf.d/99force-ipv4
+dpkg --configure -a
+apt --fix-broken install -y
+apt-get clean
+
 sudo apt-get update
-sudo apt-get install -y python3-pip git wget
+sudo apt-get install -y \
+   python3-distutils \
+   python3-setuptools \
+   python3-wheel \
+   python3-dev \
+   build-essential \
+   git \
+   wget
+
+wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
+python3 /tmp/get-pip.py --break-system-packages
+rm /tmp/get-pip.py
+
+python3 --version
+pip3 --version
+git --version
 
 #Connect to database programmatically
 wget https://dev.mysql.com/get/Downloads/MySQL-8.4/mysql-server_8.4.8-1debian13_amd64.deb-bundle.tar
@@ -32,11 +51,13 @@ pip3 install --break-system-packages -r /opt/528/hw6/requirements.txt
 
 touch /var/log/startup_already_done
 
-nohup env PROJECT_ID="${PROJECT_ID:-bucsece528}",\  
-    REGION="${REGION:-us-east5}",\
-    ZONE="${ZONE:-$REGION-a}",\
-    SQL_INSTANCE="${SQL_INSTANCE:-alhoe-hw5-mysqlinstance}",\
-    SERVICE_ACCOUNT="${SERVICE_ACCOUNT:-hw4-client-serviceaccount@${PROJECT_ID}.iam.gserviceaccount.com}",\
-    SQL_INSTANCE="${SQL_INSTANCE:-alhoe-hw5-mysqlinstance}",\
-    INSTANCE_CONNECTION_NAME="${PROJECT_ID}:${REGION}:${SQL_INSTANCE}"\
-    python3 -u /opt/528/hw6/model.py > /var/log/server.log 2>&1 &
+PROJECT_ID="${PROJECT_ID:-bucsece528}"
+REGION="${REGION:-us-east5}"
+ZONE="${ZONE:-${REGION}-a}"
+SQL_INSTANCE="${SQL_INSTANCE:-alhoe-hw5-mysqlinstance}"
+SERVICE_ACCOUNT="${SERVICE_ACCOUNT:-hw4-client-serviceaccount@${PROJECT_ID}.iam.gserviceaccount.com}"
+INSTANCE_CONNECTION_NAME="${PROJECT_ID}:${REGION}:${SQL_INSTANCE}"
+ 
+export PROJECT_ID REGION ZONE SQL_INSTANCE SERVICE_ACCOUNT INSTANCE_CONNECTION_NAME
+ 
+nohup python3 -u /opt/528/hw6/model.py > /var/log/server.log 2>&1 &
