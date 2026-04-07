9c63e75646cb443405e286905c3ca0964393ee95
Albert Hoe <albertjameshoe@gmail.com>
2026-04-07 16:51:01 -0400

    .

--- ssl_test/output.py
+++ ssl_test/output.py
@@ -5,7 +5,7 @@ bucket_name="alhoe528hw2"
 blob_name="test"
 
 
-client = storage.Client()
+client = storage.Client(project='bucsece528')
 bucket = client.bucket('alhoe528hw2')
 blob = bucket.blob(blob_name)
 blob.upload_from_string('Hello, world!')
