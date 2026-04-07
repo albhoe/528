f61694f57f3bb188b93c56282db4f00aa3040285
Albert Hoe <albertjameshoe@gmail.com>
2026-04-07 16:18:12 -0400

    .

--- ssl_test/startup.sh
+++ ssl_test/startup.sh
@@ -2,13 +2,11 @@
 
 exec > /var/log/startup-script.log 2>&1
 
-PROJECT_ID="bucsece528"
-
 apt-get update -y
 apt-get install -y python3-pip git
 git clone https://github.com/albhoe/528.git /opt/528
 pip3 install --break-system-packages -r /opt/528/ssl_test/requirements.txt
 
-gcloud config set project $PROJECT_ID
+gcloud config set project "bucsece528"
 
 nohup python3 /opt/528/ssl_test/output.py > /var/log/server.log 2>&1 &
